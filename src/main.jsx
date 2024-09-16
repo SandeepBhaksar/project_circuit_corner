73c4ffe44002fffba8b9204e3bd6e6615579a27c
Sandeep Bhaskar <ssandeepachari1@gmail.com>
2024-09-16 22:57:38 +0530

    no way

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; 
 import App from './App.jsx';
 import { Auth0Provider } from '@auth0/auth0-react';
-import './index.css';
+import './main.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); 
 
